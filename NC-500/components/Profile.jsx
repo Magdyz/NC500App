f0a63ba3150b6fd0724c3a5118a5feb7596aa8f9
TClark29 <tomclark@DESKTOP-ILEHEEH>
2024-03-25 18:37:41 +0000

    profile working

--- NC-500/components/Profile.jsx
+++ NC-500/components/Profile.jsx
@@ -1,11 +1,31 @@
-import { View, Text } from "react-native";
+import { useContext } from "react";
+import { View, Text, Button } from "react-native";
+import AuthContext from "../contexts/AuthContext";
+import supabase from "../utils/supabase";
 
-const Profile = () => {
-  return (
-    <View>
-      <Text>Profile</Text>
+
+const Profile = ({navigation}) => {
+
+  const auth = useContext(AuthContext)
+
+  function doSignOut(e){
+    return supabase.auth.signOut()
+    .then((response)=>{
+     console.log(1)
+    })
+ 
+   }
+
+ 
+    return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Profile page</Text>
+      <Button title= 'Sign Out' onPress={(e)=>doSignOut(e)}></Button>
     </View>
-  );
+
+)
+  
+  
 };
 
 export default Profile;
