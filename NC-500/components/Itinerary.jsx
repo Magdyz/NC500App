50325b3b8b0557b063d3001293da4b891bd2481f
Magz <magdy_176@hotmail.com>
2024-03-26 11:36:59 +0000

    Diconnected Things to Do from BottomBar

---
+++ NC-500/components/Itinerary.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Itinerary = () => {
+  return (
+    <View>
+      <Text>Itinerary</Text>
+    </View>
+  );
+};
+
+export default Itinerary;
