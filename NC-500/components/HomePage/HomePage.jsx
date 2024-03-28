9b94902f071445c68663fc9ffdd9dc313ba22683
Magz <magdy_176@hotmail.com>
2024-03-28 12:29:13 +0000

    Added Days navigation to Journey planner, added header and adjusted loading time for Homepage

--- NC-500/components/HomePage/HomePage.jsx
+++ NC-500/components/HomePage/HomePage.jsx
@@ -7,48 +7,56 @@ import {
   Dimensions,
 } from "react-native";
 import HomepageButton from "./HomepageButtons";
-import { Card } from "react-native-paper";
-
+import { Card, Appbar, Avatar } from "react-native-paper";
 
 const HomePage = ({ navigation }) => {
   const { height } = Dimensions.get("window");
-  const cardHeight = (height - 190 - 50 - 235 - 20) / 2; 
+  const cardHeight = (height - 190 - 0 - 200 - 20) / 2;
 
   return (
     <View style={styles.container}>
-      <View style={styles.firstRow}>
-        <HomepageButton
-          imageSource={require("../../assets/Archery.jpg")}
-          text={"Things To Do"}
-          navigation={navigation}
-          navLink={"ThingsToDo"}
+      <Appbar.Header style={styles.header}>
+        <Appbar.Content title="NC500" />
+        <Avatar.Image
+          size={60}
+          source={require("../../assets/NC500-Logo800.jpg")}
         />
-        <HomepageButton
-          imageSource={require("../../assets/aboutNC500.png")}
-          text={"About the NC500"}
-          navigation={navigation}
-          navLink={"AboutPage"}
-        />
-      </View>
-      <View style={styles.secondRow}>
-        <Card>
-          <TouchableOpacity
-            onPress={() => navigation.navigate("JourneyPlanner")}
-          >
-            <Card.Cover
-              source={require("../../assets/routePlanning.jpg")}
-              style={[styles.fullWidth, { height: cardHeight }]}
-            />
-            <Text style={styles.textOverlay}>Journey Planner</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate("Itinerary")}>
-            <Card.Cover
-              source={require("../../assets/routePlanning.jpg")}
-              style={[styles.fullWidth, { height: cardHeight }]}
-            />
-            <Text style={styles.textOverlay}>Itinerary</Text>
-          </TouchableOpacity>
-        </Card>
+      </Appbar.Header>
+      <View style={styles.body}>
+        <View style={styles.firstRow}>
+          <HomepageButton
+            imageSource={require("../../assets/Archery.jpg")}
+            text={"Things To Do"}
+            navigation={navigation}
+            navLink={"ThingsToDo"}
+          />
+          <HomepageButton
+            imageSource={require("../../assets/aboutNC500.jpg")}
+            text={"About the NC500"}
+            navigation={navigation}
+            navLink={"AboutPage"}
+          />
+        </View>
+        <View style={styles.secondRow}>
+          <Card>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("JourneyPlanner")}
+            >
+              <Card.Cover
+                source={require("../../assets/routePlanning.jpg")}
+                style={[styles.fullWidth, { height: cardHeight }]}
+              />
+              <Text style={styles.textOverlay}>Journey Planner</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate("Itinerary")}>
+              <Card.Cover
+                source={require("../../assets/routePlanning.jpg")}
+                style={[styles.fullWidth, { height: cardHeight }]}
+              />
+              <Text style={styles.textOverlay}>Itinerary</Text>
+            </TouchableOpacity>
+          </Card>
+        </View>
       </View>
     </View>
   );
@@ -56,17 +64,19 @@ const HomePage = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: "20%",
-    flex: 1,
-    paddingHorizontal: 10,
-    justifyContent: "center",
+    paddingHorizontal: 5,
+    paddingVertical: 5,
   },
+  header: {},
+  body: {},
   firstRow: {
     marginBottom: 10,
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  secondRow: {},
+  secondRow: {
+    paddingHorizontal: 10,
+  },
   fullWidth: {
     marginVertical: 5,
   },
