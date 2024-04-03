8ef7d3d3f5e273daa60164868ec95797eacbe70c
xXRea <reda.e.fizza89@gmail.com>
2024-04-03 10:17:26 +0100

    started working on the implementation of small map into things to do

--- NC-500/components/JourneyPlanner/ToDoSingleEvent.jsx
+++ NC-500/components/JourneyPlanner/ToDoSingleEvent.jsx
@@ -7,6 +7,7 @@ import {
   addLocationToRoute,
   deleteLocationFromRoute,
 } from "../../utils/supabase-api-calls";
+import SmallMap from "../SmallMap"
 
 const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
